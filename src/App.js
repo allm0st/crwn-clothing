1dae5aa979b542f06ae601bd0832bb73255e10bf
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-10-13 15:11:17 +0100

    update menu items to navigate with router hoc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
 
 function App() {
-  return <Homepage />;
+  return (
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/hats" component={HatsPage} />
+    </Switch>
+  );
 }
 
 export default App;
