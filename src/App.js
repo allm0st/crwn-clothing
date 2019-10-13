c2578eb1e00b0e99868ff9449b366466c15db49a
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-10-13 15:55:47 +0100

    adds header component and links to home and shop page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header-component.component';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={ShopPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </>
   );
 };
 
