d79dbb225ffae5ac9a7c5fe169e6d485a1c6f8cd
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-10-15 21:30:00 +0100

    implements firebase utils, includes ability to create new accounts

--- src/App.js
+++ src/App.js
@@ -5,24 +5,39 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  state = {
-    currentUser: null,
-  };
+  constructor() {
+    super();
 
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-    });
+    this.state = {
+      currentUser: null,
+    };
   }
+
+  componentDidMount() {}
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
+  unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    if (!userAuth) {
+      this.setState({ currentUser: userAuth });
+    } else {
+      const userRef = await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot(snapshot => {
+        this.setState({
+          currentUser: {
+            id: snapshot.id,
+            ...snapshot.data(),
+          },
+        });
+      });
+    }
+  });
 
   render() {
     return (
