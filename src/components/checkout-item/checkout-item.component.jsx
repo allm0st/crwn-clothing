8fe1ecc90d679ca6c91ce3a218457a8855883b94
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-10-19 23:29:17 +0100

    adds addItem and removeItem functionalities to checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  addCartItem,
+  clearCartItem,
+  removeCartItem,
+} from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, quantity, price } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
+const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => {
+  const { name, imageUrl, quantity, price } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addCartItem(item)),
+  removeItem: item => dispatch(removeCartItem(item)),
+  clearItem: item => dispatch(clearCartItem(item)),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CheckoutItem);
