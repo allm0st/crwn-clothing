7a2dbeef6c467dc5836f60f89f3e31037a6348f9
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-11-02 00:49:17 +0000

    registers serviceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from './redux/store';
-
-import './index.css';
 import App from './App';
+import './index.css';
+import { persistor, store } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,3 +18,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
